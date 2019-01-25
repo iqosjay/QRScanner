227976e66ecb918bb8aa5ffe667aa15ff81acc12
水银灯 <iqosjay@gmail.com>
2019-01-25 18:01:17 +0800

    x

--- app/src/main/java/com/iqos/test/MainActivity.java
+++ app/src/main/java/com/iqos/test/MainActivity.java
@@ -1,26 +1,83 @@
 package com.iqos.test;
 
-import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.iqos.qrscanner.app.QRScannerActivity;
 
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.VIBRATE;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String[] PERMISSIONS = new String[]{CAMERA, VIBRATE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(new String[]{Manifest.permission.CAMERA}, 1);
-        }
         setContentView(R.layout.activity_main);
     }
 
     public void openScan(View view) {
-        startActivity(new Intent(this, QRScannerActivity.class));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (null == getDeniedPms()) {
+                startActivity(new Intent(this, QRScannerActivity.class));
+            } else {
+                requestPermissions(PERMISSIONS, 1);
+            }
+        } else {
+            startActivity(new Intent(this, QRScannerActivity.class));
+        }
+    }
+
+    private String getDeniedPms() {
+        return getDeniedPms(PERMISSIONS);
+    }
+
+    private String getDeniedPms(String[] permissions) {
+        for (String permission : permissions) {
+            if (0 != checkSelfPermission(permission)) {
+                return permission;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (1 == requestCode) {
+            String deniedPermission = getDeniedPms(permissions);
+            if (null == deniedPermission) {
+                startActivity(new Intent(this, QRScannerActivity.class));
+            } else {
+                if (!shouldShowRequestPermissionRationale(deniedPermission)) {
+                    AlertDialog alertDialog = new AlertDialog.Builder(this)
+                            .setTitle("权限缺失")
+                            .setMessage("扫描二维码必须要使用相机权限")
+                            .setPositiveButton("去设置", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                    intent.setData(Uri.parse("package:" + getPackageName()));
+                                    startActivity(intent);
+                                }
+                            })
+                            .setNegativeButton("取消", null)
+                            .setCancelable(true)
+                            .create();
+                    alertDialog.setCanceledOnTouchOutside(false);
+                    alertDialog.show();
+                }
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
--- qrcodescanner/src/main/java/com/iqos/qrscanner/app/QRScannerActivity.java
+++ qrcodescanner/src/main/java/com/iqos/qrscanner/app/QRScannerActivity.java
@@ -1,14 +1,27 @@
 package com.iqos.qrscanner.app;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Vibrator;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -16,6 +29,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.Result;
@@ -23,13 +37,17 @@ import com.iqos.qrscanner.R;
 import com.iqos.qrscanner.camera.CameraManager;
 import com.iqos.qrscanner.decoding.CaptureActivityHandler;
 import com.iqos.qrscanner.decoding.InactivityTimer;
+import com.iqos.qrscanner.utils.QRCodeDecoder;
 import com.iqos.qrscanner.widget.ViewfinderView;
 
 import java.io.IOException;
 import java.util.Vector;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+
 
 public class QRScannerActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+    private static final String[] READ_GALLERY_PERMISSION = new String[]{READ_EXTERNAL_STORAGE};
     private static final long VIBRATE_DURATION = 200L;
     private static final float BEEP_VOLUME = 0.10f;
     public static final int SCAN_RESULT_CODE = 15613;
@@ -73,14 +91,6 @@ public class QRScannerActivity extends AppCompatActivity implements SurfaceHolde
         return R.layout.activity_qrscanner;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (android.R.id.home == item.getItemId()) {
-            onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     /**
      * 获取XML里面的控件
@@ -103,6 +113,49 @@ public class QRScannerActivity extends AppCompatActivity implements SurfaceHolde
             supportActionBar.setHomeButtonEnabled(true);
             supportActionBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.scan_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int i = item.getItemId();
+        if (i == android.R.id.home) {
+            this.onBackPressed();
+            return true;
+        } else if (i == R.id.open_gallery) {
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+                if (null == getDeniedPms(READ_GALLERY_PERMISSION)) {
+                    openGallery();
+                } else {
+                    requestPermissions(READ_GALLERY_PERMISSION, 300);
+                }
+            } else {
+                openGallery();
+            }
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private String getDeniedPms(String[] permissions) {
+        for (String s : READ_GALLERY_PERMISSION) {
+            if (0 != checkSelfPermission(s)) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    private void openGallery() {
+        Intent intentToPickPic = new Intent(Intent.ACTION_PICK, null);
+        intentToPickPic.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+        startActivityForResult(intentToPickPic, 2);
     }
 
     @Override
@@ -150,12 +203,40 @@ public class QRScannerActivity extends AppCompatActivity implements SurfaceHolde
      * @param result 扫描的结果
      */
     public void handleDecode(Result result) {
-        inactivityTimer.onActivity();
+        playBeepSoundAndVibrate();
+        showResult(result.getText());
+        /*inactivityTimer.onActivity();
         String resultStr = result.getText();
         Intent intent = new Intent();
         intent.putExtra(SCAN_RESULT, resultStr);
         this.setResult(SCAN_RESULT_CODE, intent);
-        finish();
+        finish();*/
+    }
+
+    private void showResult(final String result) {
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setTitle("识别结果")
+                .setMessage(result)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        restartQRScanner();
+                    }
+                })
+                .setNegativeButton("复制", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 得到剪贴板管理器
+                        ClipboardManager cmb = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                        cmb.setPrimaryClip(ClipData.newPlainText("iqosjay@gmail.com", result));
+                        Toast.makeText(QRScannerActivity.this, "复制成功", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                })
+                .setCancelable(true)
+                .create();
+        alertDialog.setCanceledOnTouchOutside(false);
+        alertDialog.show();
     }
 
     private void initCamera(SurfaceHolder surfaceHolder) {
@@ -249,4 +330,58 @@ public class QRScannerActivity extends AppCompatActivity implements SurfaceHolde
         }
     };
 
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (300 == requestCode) {
+            String pms;
+            if (null == (pms = getDeniedPms(permissions))) {
+                openGallery();
+            } else {
+                if (!shouldShowRequestPermissionRationale(pms)) {
+                    AlertDialog alertDialog = new AlertDialog.Builder(this)
+                            .setTitle("权限缺失")
+                            .setMessage("从相册读取图片必须要使用读取手机内存权限")
+                            .setPositiveButton("去设置", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                    intent.setData(Uri.parse("package:" + getPackageName()));
+                                    startActivity(intent);
+                                }
+                            })
+                            .setNegativeButton("取消", null)
+                            .setCancelable(true)
+                            .create();
+                    alertDialog.setCanceledOnTouchOutside(false);
+                    alertDialog.show();
+                }
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (2 == requestCode) {
+            if (null == data) return;
+            final Uri uri = data.getData();
+            if (null != uri) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String qrContent = QRCodeDecoder.syncDecodeQRCode(QRScannerActivity.this, uri);
+                        if (TextUtils.isEmpty(qrContent)) {
+                            Toast.makeText(QRScannerActivity.this, "未发现二维码", Toast.LENGTH_SHORT).show();
+                        } else {
+                            showResult(qrContent);
+                        }
+                    }
+                });
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
